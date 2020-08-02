6f68fdd4fe19a921fe3fdb2d19448d1dc12a2a46
Dhruv Mittal <d2mittal@edu.uwaterloo.ca>
2020-08-02 18:28:19 -0400

    added Features section

--- frontend/src/pages/Features/FeatureCard/FeatureCard.js
+++ frontend/src/pages/Features/FeatureCard/FeatureCard.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React,{useState} from 'react';
 import classes from './FeatureCard.module.css';
 
 const FeatureCard = (props) => {
+
+    const [hover, setHover] = useState(false);
+
+    const onMouseEnterHandler = () => {
+        setHover(true);
+    }
+
+    const onMouseLeaveHandler = () => {
+        setHover(false);
+    }
+
+    const normalCard = (
+        <div
+            className={classes.FeatureCard}
+            onMouseEnter={onMouseEnterHandler}
+            onMouseLeave = {onMouseLeaveHandler}
+        >
+            <h3>{props.title}</h3>
+            <div className={classes.image}>
+                <img src={props.image}/>
+            </div>
+        </div>
+    );
+
+    const hoverCard = (
+        <div 
+            className={classes.FeatureCard}
+            onMouseEnter={onMouseEnterHandler}
+            onMouseLeave = {onMouseLeaveHandler}
+        >
+            <h3>{props.title}</h3>    
+            <p>{props.description}</p>
+        </div>
+    );
+
     return (
         <div className={classes.FeatureCards}>
-            <div className={classes.FeatureCard}>
-                <h3>{props.title}</h3>
-                <div className={classes.image}>
-                    <img src={props.image}/>
-                </div>
-                <p>{props.description}</p>
-            </div>
+            {hover? hoverCard : normalCard}
         </div>
     );
 };
--- frontend/src/pages/Features/Features.js
+++ frontend/src/pages/Features/Features.js
@@ -3,12 +3,13 @@ import classes from './Features.module.css';
 import FeatureCard from './FeatureCard/FeatureCard';
 import Weather from '../../images/Weather.png';
 import Time from '../../images/Time.png';
+import Ingredients from '../../images/Ingredients.png';
 
 const Features = () => {
     return (
         <div className={classes.Features}>
-            <h1>Features</h1>
-            <h2>Get the perfect meal for your day:</h2>
+            <h1>The Perfect Meal</h1>
+            <p className={classes.Subtitle}>We account for numerous factors to give you the perfect meal recommendation</p>
             <div className={classes.FeatureCards}>
                 <FeatureCard 
                     title="Weather"
@@ -17,12 +18,13 @@ const Features = () => {
                 />
                 <FeatureCard 
                     title="Time"
-                    description="In a rush? Don't worry, we got you covered"
+                    description="In a rush? Don't worry, we can recommend recipes that's take you just 10 mins. We promise!"
                     image={Time}
                 />
                 <FeatureCard 
-                    title="Weather"
-                    description="Account for the weather, because who wants doesn't love a pinacolada on a sunny evening"
+                    title="Ingredients"
+                    image={Ingredients}
+                    description="Whether you are running out of groceries or simply feeling lazy, there is always a dish you can make"
                 />
             </div>
         </div>
